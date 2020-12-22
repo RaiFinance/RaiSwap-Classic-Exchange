827a013c265042a6ff4b307bf19e6435f8462002
Jeffrey <jeffrey@leagueoftraders.io>
2020-12-22 13:54:46 +0900

    Fixed keep break on testnet

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -10,6 +10,8 @@ import { NetworkContextName } from '../constants'
 export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & { chainId?: ChainId } {
   const context = useWeb3ReactCore<Web3Provider>()
   const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName)
+  console.log("DEBUG => context", context)
+  console.log("DEBUG => contextNetwork", contextNetwork)
   return context.active ? context : contextNetwork
 }
 
